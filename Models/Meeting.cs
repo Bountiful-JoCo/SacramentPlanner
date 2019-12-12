d045526f17cdca4230df16ed3e338b68601af11b
Courtney Toller <43355629+courtneytoller@users.noreply.github.com>
2019-12-11 23:53:57 -0700

    Add project files.

---
+++ Models/Meeting.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SacramentPlanner.Models
+{
+    public class Meeting
+    {
+        public int ID { get; set; }
+        [Display(Name = "Meeting Date")]
+        [DataType(DataType.Date)]
+        public DateTime MeetingDate { get; set; }
+
+        [StringLength(60, MinimumLength = 2)]
+        [RegularExpression("^[a-zA-Z0-9 '-]*$", ErrorMessage = "Enter name using only letters, apostrophe or hyphen")]
+        [Display(Name = "Conducting Leader")]
+        [Required]
+        public string ConductingLeader { get; set; }
+        public string OpeningSong { get; set; }
+        [StringLength(60, MinimumLength = 2)]
+        [RegularExpression("^[a-zA-Z0-9 '-]*$", ErrorMessage = "Enter name using only letters, apostrophe or hyphen")]
+        [Display(Name = "Opening Prayer")]
+        [Required]
+        public string OpeningPrayer { get; set; }
+        [Display(Name = "Sacrament Song")]
+        public string SacramentSong { get; set; }
+        [Display(Name = "Intermediate Song")]
+        public string IntermediateSong { get; set; }
+        [Display(Name = "Closing Song")]
+        public string ClosingSong { get; set; }
+        [StringLength(60, MinimumLength = 2)]
+        [RegularExpression("^[a-zA-Z0-9 '-]*$", ErrorMessage = "Enter name using only letters, apostrophe or hyphen")]
+        [Display(Name = "Closing Prayer")]
+        [Required]
+        public string ClosingPrayer { get; set; }
+
+        public ICollection<Speaker> Speakers { get; set; }
+
+    }
+}
---
+++ Models/Speaker.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SacramentPlanner.Models
+{
+    public class Speaker
+    {
+        public int ID { get; set; }
+        [StringLength(60, MinimumLength = 2)]
+        [RegularExpression("^[a-zA-Z0-9 '-]*$", ErrorMessage = "Enter Last name using only letters, apostrophe or hyphen")]
+        [Display(Name = "Last Name")]
+        [Required]
+        public string LastName { get; set; }
+        [StringLength(60, MinimumLength = 2)]
+        [RegularExpression("^[a-zA-Z0-9 '-]*$", ErrorMessage = "Enter First name using only letters, apostrophe or hyphen")]
+        [Display(Name = "First Name")]
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string Topic { get; set; }
+        [Display(Name = "Meeting Date")]
+        [DataType(DataType.Date)]
+        public DateTime SpeechDate { get; set; }
+        
+    }
+}
